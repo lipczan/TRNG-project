04121f11e079fecc5984538fd4e114b0201e7745
mauecki <maciejmalecki@int.pl>
2021-03-29 00:21:04 +0200

    test

--- main_p.m
+++ main_p.m
@@ -1,36 +1,9 @@
 %% INPUT
 clear();
 
-info = audioinfo('samples/sample.wav')
+info = audioinfo('samples/background.wav')
 % [y,Fs] = audioread('sample.wav');
-[y,Fs] = audioread('samples/sample.wav');
-
-%% BINARIZATION
-% PRÓBY BINARYZACJI DANYCH
-% fid  = fopen('background.wav', 'r');
-% data = fread(fid, [1, Inf], 'uint8');
-% fclose(fid);
-% bit = uint8(rem(floor((1 ./ [128, 64, 32, 16, 8, 4, 2, 1].') * data), 2));
-% bit = bit(:);
-
-% q = quantizer('double');
-% p = num2bin(q,y);
-
-% DoubleAsCharacters = char(typecast(y, 'uint8'));
-
-% y_abs=abs(y);
-% p=dec2bin(y_abs, 8) - '0'
-
-% wavdata = audioread('sample.wav', 'native');
-% wavbinary = uint8(reshape((dec2bin( wavdata(:), 8 ) - '0').', [], 1));
-% fid = fopen('wav.bin', 'w');
-% fwrite(fid, wavbinary, 'uint8');
-% fclose(fid);
-% fid = fopen('wav.bin', 'r');
-% A = fread(fid,'*uint8');
-% wavdata = uint16(reshape(bin2dec( char(reshape(A, 8, []).' + '0') ), [], 2));
-% SampleRate = 22100;
-% audiowrite('FileNameGoesHere.wav', wavdata, SampleRate)
+[y,Fs] = audioread('samples/background.wav');
 
 %% SIGNAL
 figure('Renderer', 'painters', 'Position', [10 10 900 600]);
@@ -61,26 +34,24 @@ B = rozmiar(2);
 N=B-1;
 % zmiana parametr?w mapy chaotycznej w zale?no?ci od pliku audio
 
-
 x(1)=x0;
-
-
 for n=1:N
     x(n+1)=r.*x(n).*(1-x(n));
 end
-% figure
-% subplot(2, 1, 1)
-% plot(x(1:N), x(2:N+1),'rs-')
-% ylabel('x(i+1)')
-% xlabel('x(i)')
-% title('x(i+1) vs x(i)')
 
-% subplot(2, 1, 2)
-% plot(x)
-% axis([0 N 0.2 1])
-% ylabel('x(i)')
-% xlabel('(i)')
-% title('x[i] vs. i')
+ figure
+ subplot(2, 1, 1)
+ plot(x(1:N), x(2:N+1),'rs-')
+ ylabel('x(i+1)')
+ xlabel('x(i)')
+
+ title('x(i+1) vs x(i)')
+ subplot(2, 1, 2)
+ plot(x)
+ axis([0 N 0.2 1])
+ ylabel('x(i)')
+ xlabel('(i)')
+ title('x[i] vs. i')
 
 
 P=2^16;
